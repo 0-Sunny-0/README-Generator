3da392cf527b6618b985e04f41ccd59378327060
Sunny Le Bunz <msafinch@gmail.com>
2024-06-30 15:05:52 -0400

    removed copy from README file, as generator will autofill that information.
    also added questions for JS generator to ask

--- index.js
+++ index.js
@@ -1,7 +1,44 @@
 // TODO: Include packages needed for this application
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+// Question 1
+    {type: "input",
+    message: "What is your Github username?",
+    name: "userName"},
+// Question 2
+    {type: "input",
+    message: "What is your email address?",
+    name: "userEmail"},
+// Question 3
+    {type: "",
+    message: "",
+    name: ""},
+// Question 4
+    {type: "",
+    message: "",
+    name: ""},
+// Question 5
+    {type: "",
+    message: "",
+    name: ""},
+// Question 6
+    {type: "",
+    message: "",
+    name: ""},
+// Question 7
+    {type: "",
+    message: "",
+    name: ""},
+// Question 8
+    {type: "",
+    message: "",
+    name: ""},
+// Question 9
+    {type: "",
+    message: "",
+    name: ""}, 
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
