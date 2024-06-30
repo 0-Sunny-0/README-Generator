220f16273d50263f05f69a7d6c0175a237872e22
Sunny Le Bunz <msafinch@gmail.com>
2024-06-30 15:24:09 -0400

    added copy back to README and added necessary package paths for index.js file.

--- README.md
+++ README.md
@@ -1,3 +1,20 @@
 # README-Generator
 
+## Description
 
+A profressional README generator, that accepts input from your terminal/CLI in order to create a README file. 
+
+## Links For Deployment
+
+<a href="https://github.com/0-Sunny-0/README-Generator">Github Repo</a><br>
+<a href="" alt="">README Generator Video</a>
+
+
+## Usage
+
+This application will be invoked using the following cammand: <br>
+node index.js <br>
+
+## Questions
+
+If you have any questions regarding this repo, please open an issue or email me directly at msafinch@gmail.com. <br> To view my other repos, take a look at my <a href="https://github.com/0-Sunny-0">GitHub profile.</a>
--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 // TODO: Include packages needed for this application
+const inquirer = require("inquirer");
+const fs = require('fs');
+const generateMarkdown = require('./generateMarkdown');
 
 // TODO: Create an array of questions for user input
 const questions = [
@@ -11,33 +14,34 @@ const questions = [
     message: "What is your email address?",
     name: "userEmail"},
 // Question 3
-    {type: "",
-    message: "",
-    name: ""},
+    {type: "input",
+    message: "What is the name of your project?",
+    name: "title"},
 // Question 4
-    {type: "",
-    message: "",
-    name: ""},
+    {type: "input",
+    message: "Write a short description about your project.",
+    name: "description"},
 // Question 5
-    {type: "",
-    message: "",
-    name: ""},
+    {type: "list",
+    message: "What type of license would you like to assign to your project?",
+    name: "license",
+    choices: ["MIT", "APACHE 2.0", "GPL 3.0", "BSD 3", "None"]},
 // Question 6
-    {type: "",
-    message: "",
-    name: ""},
+    {type: "input",
+    message: "What command should be use to run the dependencies?",
+    name: "command"},
 // Question 7
-    {type: "",
-    message: "",
-    name: ""},
+    {type: "input",
+    message: "Will there be a command for testing?",
+    name: "test"},
 // Question 8
-    {type: "",
-    message: "",
-    name: ""},
+    {type: "input",
+    message: "What usage information does a user need to know?",
+    name: "usage"},
 // Question 9
-    {type: "",
-    message: "",
-    name: ""}, 
+    {type: "input",
+    message: "What information is needed for contributing to your repo?",
+    name: "contributing"}, 
 ];
 
 // TODO: Create a function to write README file
